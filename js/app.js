18871a5ee48d02cbc4f465930edabfc39fd1ca60
Zahid Hasan <zahidhasan140338@gmail.com>
2023-09-01 11:15:32 +0600

    dynamically added four buttons

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('jo')
+const handleCategory = async() => {
+    const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+    const data = await response.json();
+    const buttonContainer = document.getElementById("btn-container");
+    data.data.forEach((category) => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <button class="btn">${category.category}</button>
+        `;
+        buttonContainer.appendChild(div);
+    })
+
+    // console.log(data.data);
+}
+
+handleCategory();
