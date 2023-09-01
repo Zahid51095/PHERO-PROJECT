5899c01d199c05646132768b61ed514badd4ed6f
Zahid Hasan <zahidhasan140338@gmail.com>
2023-09-01 11:35:01 +0600

    connected data with buttons

--- js/app.js
+++ js/app.js
@@ -5,12 +5,17 @@ const handleCategory = async() => {
     data.data.forEach((category) => {
         const div = document.createElement("div");
         div.innerHTML = `
-        <button class="btn">${category.category}</button>
+        <button onclick="handleLoadVideos('${category.category_id}')" class="btn">${category.category}</button>
         `;
         buttonContainer.appendChild(div);
-    })
+    });
 
-    // console.log(data.data);
+    console.log(data.data);
+};
+
+const handleLoadVideos = async(categoryId) => {
+    const response = await fetch(` https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await response.json();
+    console.log(data);
 }
-
 handleCategory();
